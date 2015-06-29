2a075ffe506d5b77f772363d4525dd35349bc569
zhoutingzhu <1005673016@qq.com>
2015-06-29 08:42:41 +0800

    grunt配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,26 @@
-'use strict';
-
-
-
+/**
+ * Created by Administrator on 2015/6/29.
+ */
 module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    // 加载包含 "uglify" 任务的插件。
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['uglify']);
 
 };
